684deed9f4032728acf2e76ac7fa9299f4161ab2
hapkinovich01 <hapkinovich@gmail.com>
2025-11-04 11:44:31 +0500

    Update bot_autotrader_resilient.py

--- bot_autotrader_resilient.py
+++ bot_autotrader_resilient.py
@@ -5,6 +5,7 @@ import asyncio
 import requests
 import pandas as pd
 import yfinance as yf
+import numpy as np
 import traceback
 from ta.momentum import RSIIndicator
 from ta.trend import EMAIndicator, MACD
